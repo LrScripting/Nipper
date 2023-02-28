d9616091ecdc6281cee00055746cb06cf454fef1
LrScripting <118460256+LrScripting@users.noreply.github.com>
2023-02-28 12:26:14 +0000

    Create SettingsClass.py

---
+++ src/config/SettingsClass.py
@@ -0,0 +1,37 @@
+
+class Settings:
+    def __init__(self):
+
+        self.market = {
+                "crypto": False,       
+                "symbol": "BTC/USD",
+                "tickers": ["last", 'high', 'low', 'close'],
+                "forexPair1": "USD",
+                "forexPair2": "GBP"
+        }
+        self.dataSettings =  {"timeframes": [ {"3Min": 180}, {"5Min": 300}, {"10Min": 600}, {"15Min": 900}],
+                              "dataColl": [{"high": False}, {"price": True}, {'low': False}, {"closed": False}]}
+        self.indicators = {
+                "sma": True,
+                "ema": True,
+                "rsi": False,
+                "tsi": False,
+                "Macd": True,
+                "stoch": True,
+        }
+        self.indicatorSettings = {
+                "sma": [{"s1": 1}, {"s2": 2}, {"s3": 3}],
+                "ema": [{"s1": 1}, {"s2": 2}, {"s3": 3}],
+                "rsi": [{"s1": 1}, {"s2": 2}, {"s3": 3}],
+                "tsi": [{"s1": 1}, {"s2": 2}, {"s3": 3}],
+                "macd": [{"s1": 1}, {"s2": 2}, {"s3": 3}],
+                "stoch":[{"s1": 1}, {"s2": 2}, {"s3": 3}]
+        }
+        self.tradeSettings = {
+        "sBalance": 2000,
+        "buyAmount": 500,
+        "fee": 1.001,
+        "buyThreshold": 4,
+        "buyoutRatio": 1.05,
+        "sellRatio": 0.98
+        }
